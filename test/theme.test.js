a9132c5b3c56be60199658ef949a7209d81c83a9
JBallin <JBallin@users.noreply.github.com>
2018-07-09 19:40:59 -0700

    Write passing tests for theme

---
+++ test/theme.test.js
@@ -0,0 +1,25 @@
+const { assert } = require('chai');
+const theme = require('../atom_theme/index.js');
+
+describe('theme', () => {
+  theme('d')
+
+  it('should not change from dark to dark', () => {
+    assert.equal(theme('d'), 'already set to dark theme...');
+  })
+  it('should change from dark to light', () => {
+    assert.equal(theme('l'), 'light theme!');
+  })
+  it('should change from light to dark', () => {
+    assert.equal(theme('d'), 'dark theme!');
+  })
+  it('should toggle dark to light', () => {
+    assert.equal(theme(), 'light theme!');
+  })
+  it('should toggle light to dark', () => {
+    assert.equal(theme(), 'dark theme!');
+  })
+  it('should be invalid if given wrong input', () => {
+    assert.equal(theme('wrong'), 'INVALID: "l" (light), "d" (dark) or "" (toggle)')
+  })
+})
