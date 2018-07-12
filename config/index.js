cd5db708d69449631498702049e3d3ec977dd3b7
JBallin <JBallin@users.noreply.github.com>
2018-07-11 18:08:36 -0700

    Fix getConfig to return whole config w/o args

--- config/index.js
+++ config/index.js
@@ -4,7 +4,8 @@ const HOME = require('os').homedir;
 const configJSON = fs.readFileSync(`${HOME}/.ballin-scripts/config/config.json`);
 const configObj = JSON.parse(configJSON);
 
-const getConfig = keys => keys.split('.').reduce((res, key) => res[key], configObj)
+const getConfig = keys =>
+  keys ? keys.split('.').reduce((res, key) => res[key], configObj) : configObj
 
 const setConfig = (keys, val) => {
   keysArr = keys.split('.');
@@ -14,3 +15,5 @@ const setConfig = (keys, val) => {
 }
 
 module.exports = { getConfig, setConfig };
+
+console.log(setConfig());
