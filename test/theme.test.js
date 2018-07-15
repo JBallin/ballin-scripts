4f7fa0393ea8cd910292813ffa3ce651585ab3b4
JBallin <JBallin@users.noreply.github.com>
2018-07-14 19:54:26 -0700

    Init saveTheme tests

--- test/theme.test.js
+++ test/theme.test.js
@@ -3,30 +3,35 @@ const HOME = require('os').homedir;
 const { changeTheme, saveTheme } = require(`${HOME}/.ballin-scripts/scripts/atom_theme`);
 
 describe('theme', () => {
-  let currTheme = '';
-  before('save current theme and initialize theme to dark', () => {
-    currTheme = theme('d') === 'already set to dark theme...' ? 'd' : 'l';
-  })
-  after('set theme back to current theme', () => {
-    theme(currTheme);
-  })
+  describe('changeTheme', () => {
+    let currTheme = '';
+    before('save current theme and initialize theme to dark', () => {
+      currTheme = changeTheme('d') === 'already set to dark theme...' ? 'd' : 'l';
+    })
+    after('set theme back to current theme', () => {
+      changeTheme(currTheme);
+    })
 
-  it('should not change from dark to dark', () => {
-    assert.equal(theme('d'), 'already set to dark theme...');
+    it('should not change from dark to dark', () => {
+      assert.equal(changeTheme('d'), 'already set to dark theme...');
+    })
+    it('should change from dark to light', () => {
+      assert.equal(changeTheme('l'), 'light theme!');
+    })
+    it('should change from light to dark', () => {
+      assert.equal(changeTheme('d'), 'dark theme!');
+    })
+    it('should toggle dark to light', () => {
+      assert.equal(changeTheme(), 'light theme!');
+    })
+    it('should toggle light to dark', () => {
+      assert.equal(changeTheme(), 'dark theme!');
+    })
+    it('should be invalid if given wrong input', () => {
+      assert.equal(changeTheme('wrong'), 'INVALID: "l" (light), "d" (dark) or "" (toggle)')
+    })
   })
-  it('should change from dark to light', () => {
-    assert.equal(theme('l'), 'light theme!');
-  })
-  it('should change from light to dark', () => {
-    assert.equal(theme('d'), 'dark theme!');
-  })
-  it('should toggle dark to light', () => {
-    assert.equal(theme(), 'light theme!');
-  })
-  it('should toggle light to dark', () => {
-    assert.equal(theme(), 'dark theme!');
-  })
-  it('should be invalid if given wrong input', () => {
-    assert.equal(theme('wrong'), 'INVALID: "l" (light), "d" (dark) or "" (toggle)')
+  describe('saveTheme', () => {
+    // TODO: saveTheme tests here!
   })
 })
