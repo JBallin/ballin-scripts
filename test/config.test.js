4c633dbcc83b3a95c1b44c5c9bed72ea1c2e48c3
JBallin <JBallin@users.noreply.github.com>
2018-11-12 13:57:57 -0800

    [tests] use single quotes

--- test/config.test.js
+++ test/config.test.js
@@ -26,10 +26,10 @@ const setTest = (keys, value) => {
 
 describe('config', () => {
 
-  before("fetchConfigJSON should return a String", () => {
+  before('fetchConfigJSON should return a String', () => {
     assert.typeOf(fetchConfigJSON(), 'String');
   })
-  after("tests shouldn't alter config", () => {
+  after('tests shouldn\'t alter config', () => {
     assert.equal(currentConfigJSON, fetchConfigJSON());
   })
   beforeEach('Save config', () => {
@@ -41,22 +41,22 @@ describe('config', () => {
   })
 
   describe('getConfig', () => {
-    it("('theme') should return an Object", () => {
+    it('("theme") should return an Object', () => {
       assert.isObject(getConfig('theme'))
     })
-    it("('theme.dark') should return an Array", () => {
+    it('("theme.dark") should return an Array', () => {
       assert.isArray(getConfig('theme.dark'))
     })
-    it("('gu.id') should return a String", () => {
+    it('("gu.id") should return a String', () => {
       assert.isString(getConfig('gu.id'))
     })
-    it("('up.cleanup') should return a Boolean", () => {
+    it('("up.cleanup") should return a Boolean', () => {
       assert.isBoolean(getConfig('up.cleanup'))
     })
-    it("() should return a String", () => {
+    it('() should return a String', () => {
       assert.isString(getConfig())
     })
-    it("should return an error if given keys that don't exist", () => {
+    it('should return an error if given keys that don\'t exist', () => {
       assert.equal(getConfig('wrong'), '"wrong" doesn\'t exist in config')
     })
   });
@@ -66,27 +66,26 @@ describe('config', () => {
     let savedConfig = '';
     const initialConfig = fetchConfigJSON();
 
-    after("setConfig tests shouldn't alter config", () => {
+    after('setConfig tests shouldn\'t alter config', () => {
       assert.equal(fetchConfigJSON(), initialConfig)
     })
 
-
-    it("should set theme.dark", () => {
+    it('should set theme.dark', () => {
       setTest('theme.dark', ['rainbow', 'rainbow-lite'])
     })
-    it("should set up.cleanup", () => {
+    it('should set up.cleanup', () => {
       setTest('up.cleanup', 'test');
     })
-    it("should set gu.id", () => {
+    it('should set gu.id', () => {
       setTest('gu.id', '123');
     })
-    it("should give error if given no arguments", () => {
+    it('should give error if given no arguments', () => {
       assert.equal(setConfig(), setConfigErr)
     })
-    it("should give error if given 3 arguments", () => {
+    it('should give error if given 3 arguments', () => {
       assert.equal(setConfig('a','b','c'), setConfigErr)
     })
-    it("should return the keys/value it set", () => {
+    it('should return the keys/value it set', () => {
       const keys = 'theme.light';
       const val = 'new theme';
       assert.equal(setConfig(keys, val), `"${keys}" set to "${val}"`)
@@ -94,25 +93,25 @@ describe('config', () => {
   })
 
   describe('configAction', () => {
-    it("() should return a String", () => {
+    it('() should return a String', () => {
       assert.typeOf(configAction('get'), 'string');
     })
-    it("('get') should return a String", () => {
+    it('("get") should return a String', () => {
       assert.typeOf(configAction('get'), 'string');
     })
-    it("('set') should return a setConfig error", () => {
+    it('("set") should return a setConfig error', () => {
       assert.equal(configAction('set'), setConfigErr)
     })
-    it("('get', 'gu.id') should return a Number", () => {
+    it('("get", "gu.id") should return a Number', () => {
       assert.typeOf(configAction('get', 'gu.id'), 'string')
     })
-    it("('get', 'theme.light') should return a string", () => {
+    it('("get", "theme.light") should return a string', () => {
       assert.typeOf(configAction('get', 'theme.light'), 'string')
     })
-    it("('wrong') should return an invalid error", () => {
+    it('("wrong") should return an invalid error', () => {
       assert.equal(configAction('wrong'), invalidErr);
     })
-    it("('set', 'gu.id', '123') should set gu.id to '123'", () => {
+    it('("set", "gu.id", "123") should set gu.id to "123"', () => {
       setTest('gu.id', '123', configAction);
     })
   })
