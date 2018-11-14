d88dd21f6ddb642228925e64b693d4cc3853984a
JBallin <JBallin@users.noreply.github.com>
2018-11-14 11:12:40 -0800

    [config] fix linter warnings and improve error handling

--- config/index.js
+++ config/index.js
@@ -7,81 +7,77 @@ const defaultConfigPath = path.join(__dirname, './.defaultconfig.json');
 const stringify = obj => JSON.stringify(obj, null, 2);
 
 const configMessages = {
-  actionErr: "INVALID: non-existent action given to ballin_config",
+  actionErr: 'INVALID: non-existent action given to ballin_config',
   getKeysDneErr: keys => `"${keys}" doesn't exist in config`,
-  reset: "Config has been reset to default configuration",
-  set: keys => `"${keys}" set to: ${JSON.stringify(getConfig(keys))}`,
+  reset: 'Config has been reset to default configuration',
+  set: keys => `"${keys}" set to: ${JSON.stringify(getConfig(keys))}`, // eslint-disable-line
   setArgsErr: 'INVALID: setConfig takes two arguments: "keys" and "value"',
+  getArgsErr: 'INVALID: getConfig takes two arguments: "keys" and "value"',
   setDneErr: keys => `INVALID: "${keys}" doesn't exist in config`,
-  setObjErr: (keys, prevVal) => `INVALID: "${keys} is not a bottom-level value, it returns ${JSON.stringify(prevVal)}."`
-}
+  setObjErr: (keys, prevVal) => `INVALID: "${keys} is not a bottom-level value, it returns ${JSON.stringify(prevVal)}."`,
+};
 
 const resetConfig = () => {
   const defaultConfig = fs.readFileSync(defaultConfigPath, 'utf8');
-  fs.writeFileSync(configPath, defaultConfig, 'utf8')
-}
+  fs.writeFileSync(configPath, defaultConfig, 'utf8');
+};
 
 const fetchConfig = () => {
   const configJSON = fs.readFileSync(configPath, 'utf8');
   const configObj = JSON.parse(configJSON);
-  return { configObj, configJSON }
-}
+  return { configObj, configJSON };
+};
 
-const getConfig = keys => {
+const getConfig = (keys) => {
   const { configObj, configJSON } = fetchConfig();
   if (keys !== undefined) {
-    res = keys.split('.').reduce((res, key) => res[key], configObj)
+    const res = keys.split('.').reduce((result, key) => result[key], configObj);
     return res !== undefined ? res : configMessages.getKeysDneErr(keys);
-  } else {
-    return configJSON;
-  }
-}
+  } return configJSON;
+};
 
 const setConfig = (keys, val, ...other) => {
   const { configObj } = fetchConfig();
   if (other.length || keys === undefined || val === undefined) {
     return configMessages.setArgsErr;
   }
-  keysArr = keys.split('.');
+  const keysArr = keys.split('.');
   // ex: 'theme.light' -> [ 'theme', 'light' ]
-  keyToSet = keysArr.splice(-1);
+  const keyToSet = keysArr.splice(-1);
   // 'light'
-  topLevelKeys = keysArr;
+  const topLevelKeys = keysArr;
   // [ 'theme' ]
-  nestedObj = topLevelKeys.reduce((res, key) => res[key], configObj);
+  const nestedObj = topLevelKeys.reduce((res, key) => res[key], configObj);
   // { light: 'lightTheme', dark: 'darkTheme' }
   const prevVal = nestedObj[keyToSet];
   // 'lightTheme'
 
   // make sure prevVal isn't an object (arrays and null are ok: gu.id defaults to null)
-  if (typeof prevVal === 'object' && ! Array.isArray(prevVal) && prevVal !== null) {
+  if (typeof prevVal === 'object' && !Array.isArray(prevVal) && prevVal !== null) {
     return configMessages.setObjErr(keys, prevVal);
-  } else if (prevVal === undefined) {
-    return configMessages.setDneErr(keys);
-  } else {
-    nestedObj[keyToSet] = val;
-    fs.writeFileSync(configPath, stringify(configObj), 'utf8')
-    return configMessages.set(keys);
   }
-}
+  if (!prevVal) {
+    return configMessages.setDneErr(keys);
+  }
+  nestedObj[keyToSet] = val;
+  fs.writeFileSync(configPath, stringify(configObj), 'utf8');
+  return configMessages.set(keys);
+};
 
-const configAction = (request, keys, value, other='') => {
+const configAction = (request, keys, value, other = '') => {
+  if (other) return configMessages.getArgsErr;
   if (request === 'reset') {
     resetConfig();
     return configMessages.reset;
   }
   // send config auto if not given a request with ballin_config command
-  if (request === 'get' || request === undefined) {
-    return getConfig(keys);
-  } else if (request === 'set') {
-    return setConfig(keys, value);
-  } else {
-    if (process.env.NODE_ENV !== 'test') {
-      exec('ballin', (error, stdout, stderr) => console.log(stdout));
-    }
-    return configMessages.actionErr;
+  if (request === 'get' || request === undefined) return getConfig(keys);
+  if (request === 'set') return setConfig(keys, value);
+  if (process.env.NODE_ENV !== 'test') {
+    exec('ballin', (error, stdout) => console.log(stdout)); // eslint-disable-line no-console
   }
-}
+  return configMessages.actionErr;
+};
 
 module.exports = {
   getConfig,
@@ -90,5 +86,5 @@ module.exports = {
   stringify,
   configPath,
   fetchConfig,
-  configMessages
+  configMessages,
 };
