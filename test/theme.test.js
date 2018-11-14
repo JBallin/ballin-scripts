95cc7003a9a7113066d5e6b1d86a99f49435e6f7
JBallin <JBallin@users.noreply.github.com>
2018-11-14 10:53:21 -0800

    [tests: theme] fix linter + add test saveTheme

--- test/theme.test.js
+++ test/theme.test.js
@@ -2,35 +2,38 @@ const { assert } = require('chai');
 const { changeTheme, saveTheme, themeMessages } = require('../scripts/atom_theme');
 
 describe('theme', () => {
-  describe('changeTheme', () => {
-    let currTheme = '';
-    before('save current theme and initialize theme to dark', () => {
-      currTheme = changeTheme('d') === 'already set to dark theme...' ? 'd' : 'l';
-    })
-    after('set theme back to current theme', () => {
-      changeTheme(currTheme);
-    })
+  let currThemeMode = '';
+  before('save current theme and initialize theme to dark', () => {
+    currThemeMode = changeTheme('d') === 'already set to dark theme...' ? 'd' : 'l';
+  });
+  after('set theme back to current theme', () => {
+    changeTheme(currThemeMode);
+  });
 
+  describe('changeTheme', () => {
     it('should not change from dark to dark', () => {
       assert.equal(changeTheme('d'), 'already set to dark theme...');
-    })
+    });
     it('should change from dark to light', () => {
       assert.equal(changeTheme('l'), 'light theme!');
-    })
+    });
     it('should change from light to dark', () => {
       assert.equal(changeTheme('d'), 'dark theme!');
-    })
+    });
     it('should toggle dark to light', () => {
       assert.equal(changeTheme(), 'light theme!');
-    })
+    });
     it('should toggle light to dark', () => {
       assert.equal(changeTheme(), 'dark theme!');
-    })
+    });
     it('should be invalid if given wrong input', () => {
-      assert.equal(changeTheme('wrong'), 'INVALID: "l" (light), "d" (dark) or "" (toggle)')
-    })
-  })
+      assert.equal(changeTheme('wrong'), themeMessages.argErr);
+    });
+  });
+
   describe('saveTheme', () => {
-    // TODO: saveTheme tests here!
-  })
-})
+    it('should be invalid if given wrong input', () => {
+      assert.equal(saveTheme('wrong'), themeMessages.saveErr);
+    });
+  });
+});
