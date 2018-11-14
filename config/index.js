ae20dc482c4200694bbed9f6b8236dca4a6e9ac1
JBallin <JBallin@users.noreply.github.com>
2018-11-14 13:21:59 -0800

    [config] clean up paths

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const { exec } = require('child_process');
 const path = require('path');
 
-const configPath = path.join(__dirname, './ballin.json');
-const defaultConfigPath = path.join(__dirname, './.defaultConfig.json');
+const configPath = path.join(__dirname, 'ballin.json');
+const defaultConfigPath = path.join(__dirname, '.defaultConfig.json');
 const stringify = obj => JSON.stringify(obj, null, 2);
 
 const configMessages = {
