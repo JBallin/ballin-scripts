fa89bb22bc18e799a326aad82bf4ceb613d6a86c
JBallin <JBallin@users.noreply.github.com>
2026-06-21 18:28:16 -0700

    Load test setup from Mocha config

--- test/setup.js
+++ test/setup.js
@@ -6,6 +6,7 @@ const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'ballin-config-'));
 const configPath = path.join(tempDir, 'ballin.config.json');
 const defaultConfigPath = path.join(__dirname, '..', 'config', '.defaultConfig.json');
 const previousConfigPath = process.env.BALLIN_TEST_CONFIG_PATH;
+const previousNodeEnv = process.env.NODE_ENV;
 
 const cleanup = () => {
   fs.rmSync(tempDir, { recursive: true, force: true });
@@ -19,6 +20,7 @@ try {
 }
 
 process.env.BALLIN_TEST_CONFIG_PATH = configPath;
+process.env.NODE_ENV = 'test';
 process.once('exit', cleanup);
 
 exports.mochaHooks = {
@@ -29,6 +31,11 @@ exports.mochaHooks = {
       delete process.env.BALLIN_TEST_CONFIG_PATH;
     } else {
       process.env.BALLIN_TEST_CONFIG_PATH = previousConfigPath;
+    }
+    if (previousNodeEnv === undefined) {
+      delete process.env.NODE_ENV;
+    } else {
+      process.env.NODE_ENV = previousNodeEnv;
     }
   },
 };
