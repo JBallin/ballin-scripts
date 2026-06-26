a365a6e6f2fe107d4eb92f0fe58e18bd724b2e7c
JBallin <JBallin@users.noreply.github.com>
2026-06-25 17:29:59 -0700

    Simplify ballin_config CLI output

--- config/cli.ts
+++ config/cli.ts
@@ -1,15 +1,10 @@
 const { configAction } = require('./index.ts');
 
 type ConfigCliArgs = string[];
-type WriteLine = (output: unknown) => void;
 
-const writeStdoutLine: WriteLine = (output) => {
-  console.log(output); // eslint-disable-line no-console
-};
-
-const runConfigCli = (args: ConfigCliArgs = process.argv.slice(2), writeLine: WriteLine = writeStdoutLine) => {
+const runConfigCli = (args: ConfigCliArgs = process.argv.slice(2)) => {
   const [request, keys, value, ...other] = args;
-  writeLine(configAction(request, keys, value, other));
+  console.log(configAction(request, keys, value, other)); // eslint-disable-line no-console
 };
 
 module.exports = {
